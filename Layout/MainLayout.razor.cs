ffcf227542a49f760614b629b3c0555fb11df476
Mohammed Thalha <mohammed.t@ibridgellc.com>
2023-09-15 20:39:38 +0530

    Added theme state to store in local storage of browser

--- Layout/MainLayout.razor.cs
+++ Layout/MainLayout.razor.cs
@@ -4,32 +4,41 @@ namespace ToDoListBlazorWASM.Layout;
 
 public partial class MainLayout
 {
-    private string _darkThemeIcon = "dark_mode";
-    private string _lightThemeIcon = "light_mode";
+    private readonly string _darkThemeIcon = "dark_mode";
+    private readonly string _lightThemeIcon = "light_mode";
 
-    private string _darkThemeName = "dark";
-    private string _lightThemeName = "light";
+    private readonly string _darkThemeName = "dark";
+    private readonly string _lightThemeName = "light";
 
     public string? ThemeName { get; set; }
     public string? ThemeIcon { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        var theme = await localStorageService.GetValueAsync<string>("theme");
+
+        ThemeName = theme;
+    }
 
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
         if (firstRender)
         {
-            ThemeIcon = _darkThemeIcon;
-            ThemeName = _darkThemeName;
+            ThemeName ??= _darkThemeName;
+            ThemeIcon = ThemeName == _darkThemeName ? _darkThemeIcon : _lightThemeIcon;
 
             StateHasChanged();
 
             await JS.InvokeVoidAsync("resetTooltip");
-        }       
+        }
     }
 
-    private void ToggleTheme()
+    private async Task ToggleTheme()
     {
         ThemeName = ThemeName == _darkThemeName ? _lightThemeName : _darkThemeName;
         ThemeIcon = ThemeIcon == _darkThemeIcon ? _lightThemeIcon : _darkThemeIcon;
+
+        await localStorageService.SetValueAsync<string>("theme", ThemeName);
     }
 
 }
---
+++ Services/LocalStorageService.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.JSInterop;
+
+namespace ToDoListBlazorWASM.Services;
+
+public interface ILocalStorageService
+{
+    Task<T> GetValueAsync<T>(string key);
+    Task RemoveAsync(string key);
+    Task SetValueAsync<T>(string key, T value);
+    Task Clear();
+}
+
+public class LocalStorageService : IAsyncDisposable, ILocalStorageService
+{
+    private Lazy<IJSObjectReference> _accessorJsRef = new();
+    private readonly IJSRuntime _jsRuntime;
+
+    public LocalStorageService(IJSRuntime jsRuntime)
+    {
+        _jsRuntime = jsRuntime;
+    }
+
+    private async Task WaitForReference()
+    {
+        if (_accessorJsRef.IsValueCreated is false)
+        {
+            _accessorJsRef = new(await _jsRuntime.InvokeAsync<IJSObjectReference>("import", "/js/localStorage.js"));
+        }
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        if (_accessorJsRef.IsValueCreated)
+        {
+            await _accessorJsRef.Value.DisposeAsync();
+            GC.SuppressFinalize(this);
+        }
+    }
+
+    public async Task<T> GetValueAsync<T>(string key)
+    {
+        await WaitForReference();
+        var result = await _accessorJsRef.Value.InvokeAsync<T>("get", key);
+
+        return result;
+    }
+
+    public async Task SetValueAsync<T>(string key, T value)
+    {
+        await WaitForReference();
+        await _accessorJsRef.Value.InvokeVoidAsync("set", key, value);
+    }
+
+    public async Task Clear()
+    {
+        await WaitForReference();
+        await _accessorJsRef.Value.InvokeVoidAsync("clear");
+    }
+
+    public async Task RemoveAsync(string key)
+    {
+        await WaitForReference();
+        await _accessorJsRef.Value.InvokeVoidAsync("remove", key);
+    }
+}
